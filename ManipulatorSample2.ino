7754270dd3e599a669786ef7f1473c7a54751c86
igor.grebenev@quantumsoft.ru <igor.grebenev@quantumsoft.ru>
2019-02-13 21:31:43 +0700

    first commit

---
+++ ManipulatorSample2.ino
@@ -0,0 +1,17 @@
+ // Maksim Grebenev 2018
+#import "SGServo.h"
+#import "ROT3U6DOF.h"
+
+void setup() {
+  setupROT3U6DOF(0, 0); // 500-900Initialize servos ROT3U6DOF_SERVO_COUNT/ROT3U6DOF_SERVO_START_PORT
+  resetROT3U6DOF(); // установим кутяплю в исходное положение
+  delay(6000); // подождем 1 мин для прогрева потенцииометра
+}
+
+void loop() {
+  // устанавливаем начальное положение манипулятора
+  int positions00[] = {27, 65, 120, 180, 15, 20};
+  performAllServos(positions00);
+
+  delay(3000); 
+}
